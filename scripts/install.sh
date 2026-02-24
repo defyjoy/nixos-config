43522c236b0e36b83913724638b10775d488f398
joydeep roychowdhury <defyjoy@users.noreply.github.com>
2026-02-24 06:56:36 +0530

    (chore): add installation script for NixOS setup

---
+++ scripts/install.sh
@@ -0,0 +1,23 @@
+echo "mounting /dev/sda3 to /mnt"
+sudo mount /dev/sda3 /mnt
+
+echo "installing git and chezmoi"
+sudo nix-shell -p git chezmoi
+
+echo "initializing chezmoi"
+chezmoi init --apply opsquark # this would clone into ~/.local/share/chezmoi in nixos iso home directory
+
+echo "mounting home, boot, and efivarfs"
+sudo mount --mkdir /dev/sda2 /mnt/home
+sudo mount --mkdir /dev/sda1 /mnt/boot
+sudo mount -t efivarfs efivarfs /sys/firmware/efi/efivars
+
+echo "generating hardware configuration"
+sudo nixos-generate-config --root /mnt --show-hardware-config > /mnt/etc/nixos/hardware-configuration.nix
+
+echo "installing nixos to /mnt"
+sudo nixos-install --root /mnt
+
+echo "rebooting"
+sudo reboot
+
